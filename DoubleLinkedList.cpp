46876d2ac9556997e256e451385d164db3a97bd4
AFADJARR <achmad.fadjar.ft24@mail.umy.ac.d>
2025-05-15 10:15:43 +0700

    menambahkan logika dan pengecekan duplikasi nim

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -57,6 +57,19 @@ public:
             return;
         }
 
-        // Insert to between
+        // Insert in betweennode 
+        //Step 8: Locate position for insertion
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll numbers not allowed" << endl;
+            return;
+        }
+         
     }
 }
